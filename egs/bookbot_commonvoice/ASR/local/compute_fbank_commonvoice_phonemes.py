46133d0703282ede8db828636c6e0c23f89a46a3
w11wo <wilsonwong961@gmail.com>
2023-04-04 12:19:01 +0700

    Fixed Training Script

--- egs/bookbot_commonvoice/ASR/local/compute_fbank_commonvoice_phonemes.py
+++ egs/bookbot_commonvoice/ASR/local/compute_fbank_commonvoice_phonemes.py
@@ -45,7 +45,7 @@ torch.set_num_interop_threads(1)
 def compute_fbank_commonvoice_phonemes():
     src_dir = Path("data/manifests")
     output_dir = Path("data/fbank")
-    num_jobs = min(15, os.cpu_count())
+    num_jobs = min(6, os.cpu_count())
     num_mel_bins = 80
 
     dataset_parts = (
