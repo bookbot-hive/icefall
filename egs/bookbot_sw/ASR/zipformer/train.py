b35406b096c0dbc1ab5534992b051c0ace9808e7
w11wo <davidsamuel.7878@gmail.com>
2024-03-07 06:20:34 +0000

    Implement stricter token filter

--- egs/bookbot_sw/ASR/zipformer/train.py
+++ egs/bookbot_sw/ASR/zipformer/train.py
@@ -1153,7 +1153,8 @@ def run(rank, world_size, args):
         T = ((c.num_frames - 7) // 2 + 1) // 2
         tokens = c.supervisions[0].text.split()
 
-        if T < len(tokens):
+        # https://github.com/k2-fsa/icefall/issues/1182#issuecomment-1742203390
+        if T * 2 // 3 < len(tokens):
             # logging.warning(
             #     f"Exclude cut with ID {c.id} from training. "
             #     f"Number of frames (before subsampling): {c.num_frames}. "
